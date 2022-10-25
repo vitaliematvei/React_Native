8b9432d8c00ed3f24c2d7923c0a89ed33ec2c00d
Matvei Vitalie <vitaliematvei@gmail.com>
2022-10-25 16:31:23 +0300

    Finish onPress on button

--- Transposition/App.js
+++ Transposition/App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredNoteText, setEnteredNoteText] = useState("");
+
+  https://aboutreact.com/functional-component-in-react-native/
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.noteInput}>
+        <Text style={styles.text}>
+          Enter base note (use # for sharps, eg. A#):{" "}
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="A#"
+          //onChangeText={newText => setEnteredNoteText(newText)}
+        />
+      </View>
+      <View style={styles.btnCalculate}>
+        <Button title="Calculate" color="#ffffff" />
+        {/* <Button title="Add Goal" onPress={addGoalHandler} /> */}
+      </View>
+
+      <View style={styles.noteOutput}>
+        <Text style={styles.text}>Transposed note is:</Text>
+        <Text style={styles.textInput} placeholder="---" {enteredNoteText.split(' ')} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#F0F0F0",
+    marginTop: 75,
+    padding: 20,
+  },
+  noteInput: {
+    flexDirection: "row",
+  },
+  noteOutput: {
+    marginTop: 25,
+    flexDirection: "row",
+  },
+  text: {
+    fontSize: 15,
+    padding: 8,
+  },
+  textInput: {
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "15%",
+    marginRight: 8,
+    padding: 8,
+    borderRadius: 5,
+  },
+  btnCalculate: {
+    marginTop: 25,
+    width: "50%",
+    backgroundColor: "#4630EB",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    borderRadius: 15,
   },
 });
