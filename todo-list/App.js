7b24ea41e2fbc4ab498626e36c950722ba343e13
Matvei Vitalie <vitaliematvei@gmail.com>
2022-12-27 16:15:10 +0200

    Updated the project

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import { View, StatusBar, FlatList } from "react-native";
+import { View, StatusBar, FlatList, StyleSheet } from "react-native";
 import styled from "styled-components";
 
-import AddInput from "./Components/AddInput"
-import TodoList from "./Components/TodoList"
+import AddInput from "./Components/AddInput";
+import TodoList from "./Components/TodoList";
+import Header from "./Components/Header";
+import Empty from "./Components/Empty";
 
 export default function App() {
   const [data, setData] = useState([]);
 
   const submitHandler = (value) => {
-    setData((prevTodo)=>{
-      return[
+    setData((prevTodo) => {
+      return [
         {
           value: value,
           key: Math.random().toString(),
@@ -20,28 +22,34 @@ export default function App() {
     });
   };
 
+  const deleteItem = (key) => {
+    setData((prevTodo) => {
+      return prevTodo.filter((todo) => todo.key != key);
+    });
+  };
 
   return (
     <ComponentContainer>
-      <View>
-        <StatusBar barStyle="light-content" backgroundColor="midnightblue" />
-      </View>
-
-      <View>
-        <FlatList
-          data={data}
-          renderItem={({item}) =>(
-            <TodoList item={itme} />
-          )}
-        />
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor="midnightblue" />
+      <FlatList
+        data={data}
+        ListHeaderComponent={() => <Header />}
+        ListEmptyComponent={() => <Empty />}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <TodoList item={item} deleteItem={deleteItem} />
+        )}
+      />
+      <AddInput submitHandler={submitHandler} />
     </ComponentContainer>
   );
 }
 
 const ComponentContainer = styled.View`
+  margin-top: 50px;
+  padding-top: 20px;
   background-color: midnightblue;
-  height: 100%;
+  height: 80%;
   flex-direction: column;
   align-items: center;
   justify-content: center;
--- todo-list/Components/TodoList.js
+++ todo-list/Components/TodoList.js
@@ -1,1 +1,74 @@
-// https://dev.to/reenydavidson/building-a-to-do-list-with-react-native-and-styled-components-2148#:~:text=Building%20a%20To-Do%20List%20with%20React-Native%20and%20Styled-Components.,...%207%20Final%20Touches%20...%208%20Conclusion%20
+import React from "react";
+import { View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import styled from "styled-components";
+
+export default function TodoList({ item, deleteItem }) {
+  return (
+    <ComponentContainer>
+      <ListContainer>
+        <CirlceContainer>
+          <Entypo name="circle" size={20} color="midnightblue" />
+        </CirlceContainer>
+        <View>
+          <TextItem>{item.value}</TextItem>
+          <TextDate> Task</TextDate>
+        </View>
+        <IconContainer onPress={() => deleteItem(item.key)}>
+          <MaterialIcons name="delete" size={24} color="midnightblue" />
+        </IconContainer>
+      </ListContainer>
+    </ComponentContainer>
+  );
+}
+
+const ListContainer = styled.TouchableOpacity`
+  background-color: whitesmoke;
+  height: auto;
+  width: 350px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ComponentContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  height: auto;
+  width: auto;
+`;
+
+const TextItem = styled.Text`
+  color: black;
+  width: 260px;
+  height: auto;
+  font-size: 20px;
+  margin-top: 10px;
+  margin-right: 20px;
+`;
+
+const TextDate = styled.Text`
+  color: goldenrod;
+  font-size: 15px;
+  margin-right: 20px;
+  font-family: poppins-regular;
+  border-radius: 10px;
+  width: 40px;
+`;
+
+const IconContainer = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+  margin-top: 15px;
+  height: 40px;
+  border-radius: 10px;
+`;
+
+const CirlceContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding-left: 5px;
+`;
